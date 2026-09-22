b23977901f209d46632aa5f75961f1fddd6f51b7
thejuicyseafood <iwanto.zhanghrd@gmail.com>
2026-09-21 21:29:35 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='fzqb-r3m8-16-bar-legacy-rewrite-v1';
+const CACHE='fzqb-r3m8-17-bar-repair-v1';
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
